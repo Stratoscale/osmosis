8f2322713c96f1cfed50eabc1e667a977496668e
Ygal Blum <ygal.blum@stratoscale.com>
2018-07-18 09:47:00 -0400

    Explicitly include io_service.hpp
    The includes files assume io_service.hpp is included implicitly. However,
    in new Boost version it needs to be explicitly included

--- cpp/Osmosis/cppnetlib.cpp
+++ cpp/Osmosis/cppnetlib.cpp
@@ -1,2 +1,3 @@
+#include <boost/asio/io_service.hpp>
 #include "../build/cpp-netlib-0.11.1-final/libs/network/src/client.cpp"
 #include "../build/cpp-netlib-0.11.1-final/libs/network/src/uri/uri.cpp"
