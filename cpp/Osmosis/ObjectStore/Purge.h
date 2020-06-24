32ed9258a9db0f1c4ce7ed413d52ba3ff8234e84
Eliran Levi <levioctl@gmail.com>
2020-06-24 20:26:05 +0000

    Bugfix: purge functionality (and unittest) was broken
    This reverts commit fba6b7cd91605a81afd598e5c391e23db4eaca80.
    ("Divide purge process to threads")
    
    The reverted commit is faulty and breaks purge functionality.

--- cpp/Osmosis/ObjectStore/Purge.h
+++ cpp/Osmosis/ObjectStore/Purge.h
@@ -2,7 +2,6 @@
 #define __OSMOSIS_OBJECT_STORE_PURGE_H__
 
 #include <unordered_set>
-#include <boost/filesystem.hpp>
 #include "Osmosis/ObjectStore/Labels.h"
 #include "Osmosis/ObjectStore/Store.h"
 
@@ -15,14 +14,14 @@ class Purge
 public:
 	Purge( Store & store, Labels & labels );
 
-	void purge( boost::filesystem::path & dirToPurge );
+	void purge();
 
 private:
 	Store &                     _store;
 	Labels &                    _labels;
 	std::unordered_set< Hash >  _staleHashes;
 
-	void startWithAllObjects( boost::filesystem::path & dirToPurge );
+	void startWithAllObjects();
 
 	void takeOutAllLabels();
 
